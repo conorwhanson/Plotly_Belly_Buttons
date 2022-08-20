fd88d71339fa0b72a2932e8ed648385f8a6a7371
Conor Hanson <condorhanson@gmail.com>
2022-08-20 16:13:27 -0500

    updated

--- practice/static/js/plot1.js
+++ practice/static/js/plot1.js
@@ -9,32 +9,32 @@
 // // Reverse the array due to Plotly's defaults
 // data = data.reverse();
 
-// // Trace1 for the Greek Data
-// var trace1 = {
-//   x: data.map(row => row.greekSearchResults),
-//   y: data.map(row => row.greekName),
-//   text: data.map(row => row.greekName),
-//   name: "Greek",
-//   type: "bar",
-//   orientation: "h"
-// };
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-// // data
-// var data = [trace1];
+// data
+var gods = [trace1];
 
-// // Apply the group bar mode to the layout
-// var layout = {
-//   title: "Greek gods search results",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-// };
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot("plot", data, layout);
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", gods, layout);
 
 
 // var numbers = [0,2,4,6,8];
